2d767c287f4c0a237a71e14414d7d9c26c22effa
Neuterion <nullisheree03@gmail.com>
2022-07-21 23:29:00 +0700

    changed font optimization

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,9 +7,7 @@ export default function Document() {
       <body>
         <Main/>
         <NextScript />
-        <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin='true'/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=optional" rel="stylesheet"/>
+        <style data-href="https://fonts.googleapis.com/css2?family=Inter&display=optional"></style>
       </body>
     </Html>
   )
