f5c7bada38e4f0555ef7a1312c12c542d2826b92
Neuterion <nullisheree03@gmail.com>
2022-07-31 12:30:10 +0700

    simplified tiptap comp implementation

--- components/tiptap/tiptap.jsx
+++ components/tiptap/tiptap.jsx
@@ -14,9 +14,94 @@ import {
   faAlignRight, faImage
 } from "@fortawesome/free-solid-svg-icons"
 
-import ContentStyles from './styles/editor'
+export function EditorContentStyles() {
+  return (
+    <style global jsx>{`
+      .ProseMirror {
+        padding: 0.5rem;
+        outline: none;
+      }
+      .ProseMirror > * + * {
+        margin-top: 0.5rem;
+      }
+      .ProseMirror ul, .ProseMirror ol {
+        padding: 0 1rem;
+        margin: 0.75rem 0.75rem 0 0.75rem;
+      }
+      .ProseMirror ul {
+        padding: 0 1rem;
+        list-style-type: disc;
+      }
+      .ProseMirror ol {
+        padding: 0 1rem;
+        list-style-type: decimal;
+      }
+      .ProseMirror h1 {
+        font-size: 3.75rem; /* 60px */
+        font-weight: 700;
+      }
+      .ProseMirror h2 {
+        font-size: 3rem; /* 48px */
+        font-weight: 700;
+      }
+      .ProseMirror h3 {
+        font-size: 2.25rem; /* 36px */
+        font-weight: 600;
+      }
+      .ProseMirror h4 {
+        font-size: 1.875rem; /* 30px */
+        font-weight: 600;
+      }
+      .ProseMirror h5 {
+        font-size: 1.5rem; /* 24px */
+        font-weight: 500;
+      }
+      .ProseMirror h6 {
+        font-size: 1.25rem; /* 20px */
+        font-weight: 500;
+      }
+      .ProseMirror p {
+        font-size: 1.125rem; /* 20px */
+      }
+      .ProseMirror code {
+        background-color: rgba(97, 97, 97, 0.1);
+        color: #616161;
+      }
+      .ProseMirror pre {
+        background: #0D0D0D;
+        color: #FFF;
+        font-family: 'JetBrainsMono', monospace;
+        padding: 0.75rem 1rem;
+        margin-top: 0.25rem;
+        border-radius: 0.5rem;
+      }
+      .ProseMirror pre code {
+        color: inherit;
+        padding: 0;
+        background: none;
+        font-size: 0.8rem;
+      }
+      .ProseMirror img {
+        max-width: 100%;
+        height: auto;
+        pointer-events: auto;
+        user-select: auto;
+      }
+      .ProseMirror blockquote {
+        margin-left: 1rem;
+        padding-left: 1rem;
+        border-left: 2px solid rgba(13, 13, 13, 0.1);
+      }
+      .ProseMirror hr {
+        border: none;
+        border-top: 2px solid rgba(13, 13, 13, 0.1);
+        margin: 2rem 0;
+      }
+    `}</style>
+  )
+}
 
-export const editorConfig = {
+export const EditorConfig = {
   extensions: [
     StarterKit,
     Placeholder.configure({
@@ -234,15 +319,25 @@ export const MenuButtons = ({ editor, children }) => {
   )
 }
 
-export default function Tiptap() {
-  const editor = useEditor(editorConfig)
+export const MenuBarStyles = "flex sm:justify-center flex-wrap p-2 gap-x-4 gap-y-2 bg-slate-100"
+
+export const TiptapTemplate = ({ children }) => {
   return (
     <div className="w-full flex-auto flex flex-col divide-y-2 overflow-y-auto divide-slate-900 border-2 border-slate-900" spellCheck="false">
-      <div className="flex sm:justify-center flex-wrap p-2 gap-x-4 gap-y-2 bg-slate-100">
+      { children }
+    </div>
+  )
+}
+
+export default function Tiptap() {
+  const editor = useEditor(EditorConfig)
+  return (
+    <TiptapTemplate>
+      <div className={MenuBarStyles}>
         <MenuButtons editor={editor} />
       </div>
-      <ContentStyles />
+      <EditorContentStyles />
       <EditorContent editor={editor} />
-    </div>
+    </TiptapTemplate>
   )
 }
--- pages/teacher/pengumuman/create.jsx
+++ pages/teacher/pengumuman/create.jsx
@@ -1,14 +1,59 @@
 import Head from 'next/head'
-import Tiptap, { MenuButtons } from '../../../components/tiptap/tiptap'
-import ContentStyles from '../../../components/tiptap/styles/editor'
+
+import React, { useState, useRef } from 'react'
+
+import { EditorContent, useEditor } from '@tiptap/react'
+import { TiptapTemplate, MenuBarStyles, MenuButtons, EditorConfig, EditorContentStyles } from '../../../components/tiptap/tiptap'
 
 export default () => {
+  const editor = useEditor(EditorConfig)
+
+  // Setting document title state
+  const [title, setTitle] = useState('Untitled')
+  const titleRef = useRef(null)
+  titleRef.current?.addEventListener('focusout', () => {
+    if (titleRef.current.value === '') setTitle('Untitled')
+  })
+
+  // Setting form data on submit
+  const formRef = useRef(null)
+  const contentRef = useRef(null)
+
+  // Handling form submit
+  const handleSubmit = () => {
+    contentInput.current.value = editor.getHTML()
+    titleInput.current.value = titleRef.current.value
+    form.current.submit()
+  }
+
   return (
     <main className="max-h-[100vh] flex-auto flex flex-col items-center justify-start p-4">
       <Head>
         <title>115 | Buat Pengumuman</title>
       </Head>
-      <Tiptap />
+      <form ref={formRef} action="/api/buat-pengumuman" method="post" className="flex-auto w-full max-h-full">
+        <TiptapTemplate>
+          <div className="flex flex-col w-full bg-slate-100">
+            <input
+              ref={titleRef} type="text" name="title" 
+              onChange={() => setTitle(titleRef.current.value)} value={title} autoComplete="off" 
+              className="bg-inherit h-full outline-none border-2 border-transparent focus:border-slate-200 mx-2 mt-2 py-2 text-center text-3xl italic"
+            />
+            <div className={`${MenuBarStyles} items-center content-center`}>
+              <MenuButtons editor={editor}>
+                <button onClick={handleSubmit} className="px-4 py-2 text-lg font-bold bg-blue-500 hover:bg-blue-700 text-white rounded-md">
+                  Simpan
+                </button>
+              </MenuButtons>
+            </div>
+          </div>
+          <EditorContentStyles />
+          <div class="overflow-y-scroll">
+            <EditorContent editor={editor} />
+          </div>
+          <input ref={contentRef} type="hidden" name="content" value="" />
+        </TiptapTemplate>
+      </form>
     </main>
   )
 }
@@ -26,23 +71,23 @@ export default () => {
 //   return (
 //     <div class="flex flex-col w-full bg-slate-100">
 //       { form && contentInput && titleInput ?
-//         <input 
-//           ref={titleRef} type="text" name="title" 
-//           onChange={() => setTitle(titleRef.current.value)} value={title} 
-//           autoComplete="off" className="bg-inherit h-full outline-none border-2 border-transparent focus:border-slate-200 mx-2 mt-2 py-2 text-center text-3xl italic"
-//         />
+        // <input 
+        //   ref={titleRef} type="text" name="title" 
+        //   onChange={() => setTitle(titleRef.current.value)} value={title} 
+        //   autoComplete="off" className="bg-inherit h-full outline-none border-2 border-transparent focus:border-slate-200 mx-2 mt-2 py-2 text-center text-3xl italic"
+        // />
 //         : null       
 //       }
 //       <div className="flex sm:justify-center flex-wrap p-2 gap-x-4 gap-y-2">
 //         <MenuButtons editor={editor} />
 //         { form && contentInput && titleInput ? 
-//         <button onClick={() => {
-//           contentInput.current.value = editor.getHTML()
-//           titleInput.current.value = titleRef.current.value
-//           form.current.submit()
-//         }} className="px-4 py-2 text-lg font-bold bg-blue-500 hover:bg-blue-700 text-white rounded-md">
-//           Simpan
-//         </button>
+        // <button onClick={() => {
+        //   contentInput.current.value = editor.getHTML()
+        //   titleInput.current.value = titleRef.current.value
+        //   form.current.submit()
+        // }} className="px-4 py-2 text-lg font-bold bg-blue-500 hover:bg-blue-700 text-white rounded-md">
+        //   Simpan
+        // </button>
 //         : null }
 //       </div>
 //     </div>
@@ -52,17 +97,17 @@ export default () => {
 // export default function Tiptap(isForm=false) {
 //   const editor = useEditor(editorConfig)
 //   if (isForm) {
-//     const formRef = useRef(null)
-//     const titleRef = useRef(null)
-//     const contentRef = useRef(null)
+    // const formRef = useRef(null)
+    // const titleRef = useRef(null)
+    // const contentRef = useRef(null)
 //     return (
 //       <div className="w-full flex-auto flex flex-col divide-y-2 overflow-y-auto divide-slate-900 border-2 border-slate-900" spellCheck="false">
 //         <MenuBar editor={editor} form={formRef} titleInput={titleRef} contentInput={contentRef} />
 //         <ContentStyles />
 //         <form ref={formRef} action="/api/buat-pengumuman" method="post" className="overflow-y-auto w-full">
 //           <EditorContent editor={editor} />
-//           <input ref={titleRef} type="hidden" name="title" value="" />
-//           <input ref={contentRef} type="hidden" name="content" value="" />
+          // <input ref={titleRef} type="hidden" name="title" value="" />
+          // <input ref={contentRef} type="hidden" name="content" value="" />
 //         </form>
 //       </div>
 //     )
